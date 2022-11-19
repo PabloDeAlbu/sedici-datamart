0bc53329f51e94eda0c97fc93bbb030b730d206b
Pablo de Albuquerque <pablo.cda@gmail.com>
2022-11-19 13:48:31 -0300

    Agrego notebook para instituciones y filiaciones de Scopus
     También agrego carpeta donde se ubican las configuraciones
     y un .gitignore

--- dim_institucion/scopus.py
+++
@@ -1,7 +0,0 @@
-import pybliometrics
-# c1a9a0aa724682d53a7883b3db11644b
-pybliometrics.scopus.utils.create_config()
-
-from pybliometrics.scopus import AffiliationRetrieval
-aff1 = AffiliationRetrieval(60032057)
-aff1.author_count
