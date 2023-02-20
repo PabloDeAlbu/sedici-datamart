918b208a0cf31bd8639487b38d29a4e5374e027e
Pablo de Albuquerque <pablo.cda@gmail.com>
2023-02-20 20:21:08 -0300

    first load dim_institution

--- load/dim_institution/loader.py
+++ load/dim_institution/loader.py
@@ -2,56 +2,59 @@ from decouple import config
 import pandas as pd
 from datetime import date
 import pdb
+import psycopg2
+import psycopg2.extras
 
 from sqlalchemy import create_engine
-import pyodbc
 
-filepath = f"{config('filepath')}_{date.today()}"
+filepath = config('filepath')
 STAGING_DIR = config('STAGING_DIR')
 
 DWH_HOST=config('DWH_HOST')
-DWH_PORT=config('DWH_PORT')
 DWH_DB=config('DWH_DB')
 DWH_USER=config('DWH_USER')
 DWH_PASSWORD=config('DWH_PASSWORD')
+DWH_PORT=config('DWH_PORT')
+DWH_TABLE = config('DWH_TABLE')
+
+conn = None
+cur = None
+
+conn_string = f'postgresql://{DWH_USER}:{DWH_PASSWORD}@{DWH_HOST}:{DWH_PORT}/{DWH_DB}'
+
 
 df = pd.read_parquet(filepath)
 
-def load():
-    # Create an engine instance
-    alchemyEngine = create_engine(f'postgresql://{DWH_USER}:{DWH_PASSWORD}@{DWH_HOST}:{DWH_PORT}/{DWH_DB}')
-    
-    # Connect to PostgreSQL server
-    dbConnection = alchemyEngine.connect()
-    
-    # Read data from PostgreSQL database table and load into a DataFrame instance
-    dataFrame = pd.read_sql("select * from \"general.dim_institution\"", dbConnection)
-    
-    pd.set_option('display.expand_frame_repr', False)
-    
-    # Print the DataFrame
-    print(dataFrame)
-    
-    # Close the database connection
-    dbConnection.close()
-    breakpoint()
-
-#load data to postgres
-def load(df, tbl):
-    try:
-        rows_imported = 0
-        engine = create_engine(f'postgresql://{DWH_USER}:{DWH_PASSWORD}@{DWH_HOST}:{DWH_PORT}/{DWH_DB}')
- 
-        print(f'importing rows {rows_imported} to {rows_imported + len(df)}... ')
-        # save df to postgres
-        df.to_sql(f"stg_{tbl}", engine, if_exists='replace', index=False)
-        rows_imported += len(df)
-        # add elapsed time to final print out
-        print("Data imported successful")
-    except Exception as e:
-        print("Data load error: " + str(e))
+records = []
+for index, row in df.iterrows():
+    records.append(tuple(row))
 
 try:
-    load()
-except Exception as e:
-    print("Data load error: " + str(e))
+    with psycopg2.connect(
+                host = DWH_HOST,
+                dbname = DWH_DB,
+                user = DWH_USER,
+                password = DWH_PASSWORD,
+                port = DWH_PORT
+                ) as conn:
+
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+
+            cur.fast_executemany = True   # reduce number of calls to server on inserts
+
+            # form SQL statement
+            columns = ", ".join(df.columns)
+            values = '('+', '.join(['%s']*len(df.columns))+')'
+            statement = f"INSERT INTO {DWH_TABLE} ({columns}) VALUES {values}"
+            # extract values from DataFrame into list of tuples
+            insert = [tuple(x) for x in df.values]
+
+            cur.executemany(statement, insert)
+
+
+except Exception as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
--- transform/dim_institution/transformer.py
+++ transform/dim_institution/transformer.py
@@ -8,6 +8,65 @@ STAGING_DIR = config('STAGING_DIR')
 
 df = pd.read_csv(filepath_or_buffer=filepath, sep=";")
 
-df['es_doble_dependencia'] = df['es_doble_dependencia'].replace('N', 'S') 
+df.rename(
+    columns={
+        'sigla': 'abbreviation',
+        'es_doble_dependencia': 'sicytar_es_doble_dependencia',
+        'institucion_nivel1_id': 'sicytar_institucion_nivel1_id',
+        'institucion_nivel2_id': 'sicytar_institucion_nivel2_id',
+        'institucion_nivel3_id': 'sicytar_institucion_nivel3_id',
+        'institucion_nivel4_id': 'sicytar_institucion_nivel4_id',
+        'institucion_nivel5_id': 'sicytar_institucion_nivel5_id',
+        'institucion_nivel6_id': 'sicytar_institucion_nivel6_id',
+        'organizacion_id': 'sicytar_organizacion_id',
+        'institucion_nivel1_descripcion': 'sicytar_institucion_nivel1_descripcion',
+        'institucion_nivel2_descripcion': 'sicytar_institucion_nivel2_descripcion',
+        'institucion_nivel3_descripcion': 'sicytar_institucion_nivel3_descripcion',
+        'institucion_nivel4_descripcion': 'sicytar_institucion_nivel4_descripcion',
+        'institucion_nivel5_descripcion': 'sicytar_institucion_nivel5_descripcion',
+        'institucion_nivel6_descripcion': 'sicytar_institucion_nivel6_descripcion',
+        'nivel': 'sicytar_nivel'
+    },
+    inplace=True
+)
 
-df.to_parquet(f'{STAGING_DIR}/scopus_authors_{date.today()}.parquet.gzip', compression='gzip')
+df = df.drop(['tipo_entidad_privada_id', 'estado_organizacion_id', 'tipo_institucion_id'], axis=1)
+
+int_columns = [
+    'sicytar_institucion_nivel1_id', 
+    'sicytar_institucion_nivel2_id',
+    'sicytar_institucion_nivel3_id', 
+    'sicytar_institucion_nivel4_id', 
+    'sicytar_institucion_nivel5_id', 
+    'sicytar_institucion_nivel6_id', 
+    'sicytar_organizacion_id', 
+    'sicytar_nivel',
+    ]
+
+df[int_columns] = df[int_columns].fillna(0)
+
+str_columns = [
+    'abbreviation',
+    'sicytar_institucion_nivel1_descripcion',
+    'sicytar_institucion_nivel2_descripcion',
+    'sicytar_institucion_nivel3_descripcion',
+    'sicytar_institucion_nivel4_descripcion',
+    'sicytar_institucion_nivel5_descripcion',
+    'sicytar_institucion_nivel6_descripcion',
+    'sicytar_es_doble_dependencia',
+]
+
+df[str_columns] = df[str_columns].fillna('No Informado')
+
+convert_dict = {}
+
+for i in str_columns:
+    convert_dict[i] = pd.StringDtype()
+
+for i in int_columns:
+    convert_dict[i] = int
+
+df = df.astype(convert_dict)
+
+parquet_engine = 'pyarrow'
+df.to_parquet(f'{STAGING_DIR}/dim_institution_{date.today()}.parquet.gzip', engine = parquet_engine, compression='gzip')
